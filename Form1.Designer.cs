17fbda8561f0efe38358d0ae29d5de68fd1f8272
MDan9382 <160198160+MDan9382@users.noreply.github.com>
2024-05-25 11:14:02 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace tarea_en_clase_p1_c16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelDaniel = new System.Windows.Forms.Label();
+            this.labelIsai = new System.Windows.Forms.Label();
+            this.labelnombre = new System.Windows.Forms.Label();
+            this.textBoxnombre = new System.Windows.Forms.TextBox();
+            this.labelcompania = new System.Windows.Forms.Label();
+            this.labelanio = new System.Windows.Forms.Label();
+            this.labelgeneracion = new System.Windows.Forms.Label();
+            this.numericUpDownanio = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDowngeneracion = new System.Windows.Forms.NumericUpDown();
+            this.dataGridViewconsolas = new System.Windows.Forms.DataGridView();
+            this.buttoninsertar = new System.Windows.Forms.Button();
+            this.buttonactualizar = new System.Windows.Forms.Button();
+            this.comboBoxCompania = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownanio)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDowngeneracion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewconsolas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelDaniel
+            // 
+            this.labelDaniel.AutoSize = true;
+            this.labelDaniel.Font = new System.Drawing.Font("Rockwell", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDaniel.Location = new System.Drawing.Point(23, 13);
+            this.labelDaniel.Name = "labelDaniel";
+            this.labelDaniel.Size = new System.Drawing.Size(598, 31);
+            this.labelDaniel.TabIndex = 0;
+            this.labelDaniel.Text = "Mario Daniel Esquivel Sandoval 0905-22-11866";
+            // 
+            // labelIsai
+            // 
+            this.labelIsai.AutoSize = true;
+            this.labelIsai.Font = new System.Drawing.Font("Rockwell", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelIsai.Location = new System.Drawing.Point(23, 44);
+            this.labelIsai.Name = "labelIsai";
+            this.labelIsai.Size = new System.Drawing.Size(560, 31);
+            this.labelIsai.TabIndex = 1;
+            this.labelIsai.Text = "Mario Isaí Esquivel Sandoval 0905-22-11874";
+            // 
+            // labelnombre
+            // 
+            this.labelnombre.AutoSize = true;
+            this.labelnombre.Location = new System.Drawing.Point(29, 91);
+            this.labelnombre.Name = "labelnombre";
+            this.labelnombre.Size = new System.Drawing.Size(42, 13);
+            this.labelnombre.TabIndex = 2;
+            this.labelnombre.Text = "nombre";
+            // 
+            // textBoxnombre
+            // 
+            this.textBoxnombre.Location = new System.Drawing.Point(88, 88);
+            this.textBoxnombre.Name = "textBoxnombre";
+            this.textBoxnombre.Size = new System.Drawing.Size(181, 20);
+            this.textBoxnombre.TabIndex = 3;
+            // 
+            // labelcompania
+            // 
+            this.labelcompania.AutoSize = true;
+            this.labelcompania.Location = new System.Drawing.Point(26, 127);
+            this.labelcompania.Name = "labelcompania";
+            this.labelcompania.Size = new System.Drawing.Size(53, 13);
+            this.labelcompania.TabIndex = 4;
+            this.labelcompania.Text = "compania";
+            // 
+            // labelanio
+            // 
+            this.labelanio.AutoSize = true;
+            this.labelanio.Location = new System.Drawing.Point(29, 164);
+            this.labelanio.Name = "labelanio";
+            this.labelanio.Size = new System.Drawing.Size(101, 13);
+            this.labelanio.TabIndex = 5;
+            this.labelanio.Text = "anio de lanzamiento";
+            // 
+            // labelgeneracion
+            // 
+            this.labelgeneracion.AutoSize = true;
+            this.labelgeneracion.Location = new System.Drawing.Point(29, 198);
+            this.labelgeneracion.Name = "labelgeneracion";
+            this.labelgeneracion.Size = new System.Drawing.Size(60, 13);
+            this.labelgeneracion.TabIndex = 6;
+            this.labelgeneracion.Text = "geenracion";
+            // 
+            // numericUpDownanio
+            // 
+            this.numericUpDownanio.Location = new System.Drawing.Point(159, 164);
+            this.numericUpDownanio.Maximum = new decimal(new int[] {
+            2025,
+            0,
+            0,
+            0});
+            this.numericUpDownanio.Minimum = new decimal(new int[] {
+            1970,
+            0,
+            0,
+            0});
+            this.numericUpDownanio.Name = "numericUpDownanio";
+            this.numericUpDownanio.Size = new System.Drawing.Size(124, 20);
+            this.numericUpDownanio.TabIndex = 8;
+            this.numericUpDownanio.Value = new decimal(new int[] {
+            1970,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDowngeneracion
+            // 
+            this.numericUpDowngeneracion.Location = new System.Drawing.Point(116, 198);
+            this.numericUpDowngeneracion.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDowngeneracion.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDowngeneracion.Name = "numericUpDowngeneracion";
+            this.numericUpDowngeneracion.Size = new System.Drawing.Size(68, 20);
+            this.numericUpDowngeneracion.TabIndex = 9;
+            this.numericUpDowngeneracion.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // dataGridViewconsolas
+            // 
+            this.dataGridViewconsolas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewconsolas.Location = new System.Drawing.Point(29, 243);
+            this.dataGridViewconsolas.Name = "dataGridViewconsolas";
+            this.dataGridViewconsolas.Size = new System.Drawing.Size(544, 107);
+            this.dataGridViewconsolas.TabIndex = 10;
+            // 
+            // buttoninsertar
+            // 
+            this.buttoninsertar.Location = new System.Drawing.Point(453, 88);
+            this.buttoninsertar.Name = "buttoninsertar";
+            this.buttoninsertar.Size = new System.Drawing.Size(75, 23);
+            this.buttoninsertar.TabIndex = 11;
+            this.buttoninsertar.Text = "insertar";
+            this.buttoninsertar.UseVisualStyleBackColor = true;
+            this.buttoninsertar.Click += new System.EventHandler(this.buttoninsertar_Click);
+            // 
+            // buttonactualizar
+            // 
+            this.buttonactualizar.Location = new System.Drawing.Point(453, 127);
+            this.buttonactualizar.Name = "buttonactualizar";
+            this.buttonactualizar.Size = new System.Drawing.Size(75, 23);
+            this.buttonactualizar.TabIndex = 12;
+            this.buttonactualizar.Text = "actualizar";
+            this.buttonactualizar.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxCompania
+            // 
+            this.comboBoxCompania.FormattingEnabled = true;
+            this.comboBoxCompania.Items.AddRange(new object[] {
+            "atari",
+            "coleco",
+            "mattel",
+            "microsoft",
+            "nintendo",
+            "ouya inc.",
+            "sega",
+            "sony"});
+            this.comboBoxCompania.Location = new System.Drawing.Point(88, 127);
+            this.comboBoxCompania.Name = "comboBoxCompania";
+            this.comboBoxCompania.Size = new System.Drawing.Size(181, 21);
+            this.comboBoxCompania.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxCompania);
+            this.Controls.Add(this.buttonactualizar);
+            this.Controls.Add(this.buttoninsertar);
+            this.Controls.Add(this.dataGridViewconsolas);
+            this.Controls.Add(this.numericUpDowngeneracion);
+            this.Controls.Add(this.numericUpDownanio);
+            this.Controls.Add(this.labelgeneracion);
+            this.Controls.Add(this.labelanio);
+            this.Controls.Add(this.labelcompania);
+            this.Controls.Add(this.textBoxnombre);
+            this.Controls.Add(this.labelnombre);
+            this.Controls.Add(this.labelIsai);
+            this.Controls.Add(this.labelDaniel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownanio)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDowngeneracion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewconsolas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDaniel;
+        private System.Windows.Forms.Label labelIsai;
+        private System.Windows.Forms.Label labelnombre;
+        private System.Windows.Forms.TextBox textBoxnombre;
+        private System.Windows.Forms.Label labelcompania;
+        private System.Windows.Forms.Label labelanio;
+        private System.Windows.Forms.Label labelgeneracion;
+        private System.Windows.Forms.NumericUpDown numericUpDownanio;
+        private System.Windows.Forms.NumericUpDown numericUpDowngeneracion;
+        private System.Windows.Forms.DataGridView dataGridViewconsolas;
+        private System.Windows.Forms.Button buttoninsertar;
+        private System.Windows.Forms.Button buttonactualizar;
+        private System.Windows.Forms.ComboBox comboBoxCompania;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tarea_en_clase_p1_c16.Datos.Modelos;
+using tarea_en_clase_p1_c16.Datos;
+
+namespace tarea_en_clase_p1_c16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            coneccion con = new coneccion();
+            con.pruebaConneccion();
+        }
+
+        private void buttoninsertar_Click(object sender, EventArgs e)
+        {
+            consola_parametros consola = new consola_parametros(textBoxnombre.Text,comboBoxCompania.Text,numericUpDownanio.Value, numericUpDowngeneracion.Value);
+        }
+    }
+}
